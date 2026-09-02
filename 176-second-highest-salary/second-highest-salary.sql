b2500c45e6c6ecba3ffd5c695288102184308d43
Venkateswara Reddy K <167555067+VenkyK2307@users.noreply.github.com>
2026-09-02 11:23:46 +0530

    Time: 271 ms (86.14%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,12 +1,2 @@
-with salaries as (
-
-    select id , salary ,
-    dense_rank() over (order by salary desc) as ranking
-    from employee
-
-
-)
-
-select (
-    select salary from salaries where ranking = 2 limit 1
-) as SecondHighestSalary 
+select max(salary) as SecondHighestSalary from employee 
+where salary < (select max(salary) from employee)
